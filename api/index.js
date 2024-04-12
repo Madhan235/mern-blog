7289045754aa526362d8fb53978c284492726d7f
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-04-12 16:42:21 +0530

    update in progress

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import moongoose from 'mongoose';
 import dotenv from 'dotenv';
 import  userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ moongoose.connect(process.env.MONGO)
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000,()=> console.log('server is running on port 3000'));
 
