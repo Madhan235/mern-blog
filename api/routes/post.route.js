edc4ee9b7ae1ead8489b58d510597db5aef308dc
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-04-16 17:56:54 +0530

    update post funcationality compeleted

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js'
-import { createPost, deletepost, getposts } from '../controllers/post.controllers.js';
+import { createPost, deletepost, getposts, updatepost } from '../controllers/post.controllers.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/create', verifyToken, createPost );
 
 router.get('/getposts', getposts);
 router.delete('/delete/:postId/:userId', verifyToken,deletepost);
+router.put('/update/:postId/:userId', verifyToken, updatepost)
 
 export default router
